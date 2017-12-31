b3f4f2e04a09ff17cf16c4a3b72164206e28844e
JosephStevens <josephscottstevens@gmail.com>
2017-12-31 14:13:28 -0500

    added image, initial commit

--- Chat.elm
+++ Chat.elm
@@ -1,33 +1,37 @@
 module Chat exposing (..)
 
-import Color exposing (rgba)
-import Element exposing (..)
-import Element.Attributes exposing (..)
-import Style exposing (..)
+import Color
+import Element
+import Element.Attributes as Attributes
+import Style exposing (style)
 import Style.Color as Color
 import Style.Font as Font
 
 
 type Styles
     = None
-    | Label
-    | Navbar
     | Main
+    | Navbar
 
 
 colors =
-    { focusedText = rgba 51 51 51 1
-    , outOfFocusText = rgba 187 187 187 1
+    { focused = Color.rgba 51 51 51 1
+    , outOfFocus = Color.rgba 187 187 187 1
     }
 
 
-stylesheet : StyleSheet Styles variation
+stylesheet : Style.StyleSheet Styles variation
 stylesheet =
     Style.styleSheet
         [ style None []
         , style Navbar
             [ Color.background Color.white
+            , Color.text colors.outOfFocus
+            , Font.typeface
+                [ Font.importUrl { url = "https://fonts.googleapis.com/css", name = "Montserrat" }
+                ]
             , Font.typeface [ Font.font "Montserrat" ]
+            , Font.size 16
             ]
         , style Main
             []
@@ -36,26 +40,32 @@ stylesheet =
 
 main =
     Element.viewport stylesheet <|
-        column Main
-            [ height fill ]
-            [ row Navbar [] navbar
-            , row None
-                [ height fill
-                , width fill
+        Element.column Navbar
+            [ Attributes.width Attributes.fill ]
+            [ Element.row None
+                [ Attributes.spacing 20, Attributes.center, Attributes.padding 20 ]
+                [ Element.el None [] (Element.text "HOME")
+                , Element.el None [] (Element.text "CONNECT")
+                , Element.el None [] (Element.text "FOOD")
+                , Element.el None [] (Element.text "DRINKS")
+                , Element.el None [] (Element.text "WEEKLY SPECIALS")
+                , Element.el None [] (Element.text "SEASONAL SPECIALS")
+                , Element.el None [] (Element.text "OUR STORY")
+                , Element.el None [] (Element.text "OUR COFFEE")
+                , Element.el None [] (Element.text "ART SHOWS")
+                , Element.el None [] (Element.text "PHOTOS")
+                , Element.el None [] (Element.text "CONNECT")
                 ]
-                [ body ]
+            , Element.row None
+                []
+                [ Element.image None [ Attributes.width (Attributes.percent 100), Attributes.maxHeight (Attributes.px 520) ] connectImage
+                ]
             ]
 
 
-navbar =
-    [ el Navbar [ padding 20 ] (text "Navbar")
-    , el Navbar [ padding 20 ] (text "Testbar")
-    ]
+
+-- connectImage is too big for mobile
 
 
-body =
-    column None
-        [ alignLeft
-        , width fill
-        ]
-        []
+connectImage =
+    { src = "Organized-Fashion.jpg", caption = "CONNECT" }
