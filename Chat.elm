01affb39e560b9d24624b14fa418f9931850e9d4
JosephStevens <josephscottstevens@gmail.com>
2018-01-27 13:42:26 -0500

    started on hours, added notes

--- Chat.elm
+++ Chat.elm
@@ -11,12 +11,13 @@ import Style.Font as Font
 type Styles
     = None
     | Main
-    | Navbar
+    | TextHeader
 
 
 colors =
     { focused = Color.rgba 51 51 51 1
     , outOfFocus = Color.rgba 187 187 187 1
+    , textHeader = Color.rgb 102 108 112
     }
 
 
@@ -27,13 +28,15 @@ stylesheet =
         , style Main
             [ Color.background Color.white
             , Color.text colors.outOfFocus
-            , Font.typeface
-                [ Font.importUrl { url = "https://fonts.googleapis.com/css?family=Montserrat", name = "Montserrat" }
-                ]
-            , Font.typeface [ Font.font "Montserrat" ]
+            , Font.typeface [ Font.importUrl { url = "https://fonts.googleapis.com/css?family=Montserrat", name = "Montserrat" } ]
             , Font.size 16
             ]
-        , style Navbar []
+        , style TextHeader
+            [ Font.capitalize
+            , Font.underline
+            , Font.size 20
+            , Color.text colors.textHeader
+            ]
         ]
 
 
@@ -61,14 +64,14 @@ main =
                     [ Attributes.width (Attributes.percent 100)
                     , Attributes.maxHeight (Attributes.px 520)
                     ]
-                    connectImage
+                    { src = "Organized-Fashion.jpg", caption = "CONNECT" }
+                ]
+            , Element.row None
+                [ Attributes.center
+                , Attributes.minWidth (Attributes.px 1000)
+                ]
+                [ Element.row TextHeader [] [ Element.text "HOURS" ]
+                , Element.row None [] [ Element.text "Mon-Sat: 8am-3pm (breakfast until 10:30am)" ]
+                , Element.row None [] [ Element.text "Sunday:     Closed" ]
                 ]
             ]
-
-
-
--- connectImage is too big for mobile
-
-
-connectImage =
-    { src = "Organized-Fashion.jpg", caption = "CONNECT" }
